6ecf3863b6c9b488c4c32a6faf109bfe026fc3a6
torio <raikan327@icloud.com>
2024-10-24 21:37:58 +0900

    feat : add chat function

--- index.js
+++ index.js
@@ -11,14 +11,20 @@ const io=new Server(server);
 
 const __dirname=dirname(fileURLToPath(import.meta.url))
 
-app.get('/',(req,res)=>{
-    res.sendFile(join(__dirname,'index.html'))
-})
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'));
+});
+
+//接続を作成
 
 io.on('connection',(socket)=>{
-    console.log('a user connected');
+    console.log("user connected")
+    socket.on('chat message',(msg)=>{
+        console.log('message:'+msg)
+        io.emit('chat message',msg);
+    })
 })
 
-server.listen(3000,()=>{
-    console.log('server running at http://localhost:3000');
+server.listen(3005,()=>{
+    console.log('server running at http://localhost:3005');
 })
