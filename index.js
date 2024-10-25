2d2aa62ef43bc73655ff8e526eee511c497b825e
torio <raikan327@icloud.com>
2024-10-25 21:22:43 +0900

    study socket.io

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ import {createServer} from 'node:http';
 import {fileURLToPath} from 'node:url';
 import {dirname,join} from 'node:path';
 import {Server} from 'socket.io';
+import sqlite3 from 'sqlite3'
+import {open} from "sqlite";
+
+const db=await open({
+    filename:'chat.db',
+    driver:sqlite3.Database
+})
+
+await db.exec(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        client_offset TEXT UNIQUE,
+        content TEXT
+    )
+`)
 
 
 const app=express()
@@ -17,12 +32,30 @@ app.get('/', (req, res) => {
 
 //接続を作成
 
-io.on('connection',(socket)=>{
+io.on('connection',async (socket)=>{
     console.log("user connected")
-    socket.on('chat message',(msg)=>{
-        console.log('message:'+msg)
-        io.emit('chat message',msg);
+    socket.on('chat message',async (msg)=>{
+        let result;
+        try{
+            result=await db.run('INSERT INTO messages (content) VALUES (?)',msg);
+        }catch (e) {
+            return;
+        }
+        io.emit('chat message',msg,result.lastID);
     })
+    
+    if(!socket.recovered){
+        try{
+            await db.each('SELECT id,content FROM messages WHERE id >?'
+            [socket.handshake.auth.serverOffset||0],
+                (_err,row)=>{
+                socket.emit('chat message',row.content,row.id);
+                }
+            )
+        }catch (e) {
+
+        }
+    }
 })
 
 server.listen(3005,()=>{
