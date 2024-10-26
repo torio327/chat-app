06ee1ae6049238eb21ae3adf10be694c1dd428b5
torio <raikan327@icloud.com>
2024-10-27 06:08:06 +0900

    complete

--- index.js
+++ index.js
@@ -5,24 +5,41 @@ import {dirname,join} from 'node:path';
 import {Server} from 'socket.io';
 import sqlite3 from 'sqlite3'
 import {open} from "sqlite";
+import {availableParallelism} from 'node:os';
+import cluster from "node:cluster";
+import {createAdapter,setupPrimary} from '@socket.io/cluster-adapter';
 
-const db=await open({
-    filename:'chat.db',
-    driver:sqlite3.Database
-})
 
-await db.exec(`
+
+if(cluster.isPrimary){
+    const numCPUs=availableParallelism()
+
+    for(let i=0;i<numCPUs;i++){
+        cluster.fork({
+            PORT:3000+i
+        });
+    }
+
+    setupPrimary()
+}else {
+    const db=await open({
+        filename:'chat.db',
+        driver:sqlite3.Database
+    })
+
+    await db.exec(`
     CREATE TABLE IF NOT EXISTS messages (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         client_offset TEXT UNIQUE,
         content TEXT
     )
 `)
-
-
 const app=express()
 const server=createServer(app);
-const io=new Server(server);
+const io=new Server(server,{
+    connectionStateRecovery:{},
+    adapter:createAdapter()
+});
 
 const __dirname=dirname(fileURLToPath(import.meta.url))
 
@@ -34,16 +51,22 @@ app.get('/', (req, res) => {
 
 io.on('connection',async (socket)=>{
     console.log("user connected")
-    socket.on('chat message',async (msg)=>{
+    socket.on('chat message',async (msg,clientOffset,callback)=>{
         let result;
         try{
-            result=await db.run('INSERT INTO messages (content) VALUES (?)',msg);
+            result=await db.run('INSERT INTO messages (content,client_offset) VALUES (?, ?)',msg,clientOffset);
         }catch (e) {
+            if(e.errno===19){
+                callback()
+            }else{
+
+            }
             return;
         }
         io.emit('chat message',msg,result.lastID);
+        callback()
     })
-    
+
     if(!socket.recovered){
         try{
             await db.each('SELECT id,content FROM messages WHERE id >?'
@@ -57,7 +80,11 @@ io.on('connection',async (socket)=>{
         }
     }
 })
+const port=process.env.PORT;
 
-server.listen(3005,()=>{
-    console.log('server running at http://localhost:3005');
+server.listen(port,()=>{
+    console.log(`server running at http://localhost:${port}`);
 })
+
+}
+
